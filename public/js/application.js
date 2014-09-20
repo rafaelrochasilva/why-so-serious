b1130462dbd838660047815edf043e4ab0de5571
Rafael Rocha <null>
2014-09-20 11:37:06 -0400

    Get the current image and send 3 diffent images to the server

--- public/js/application.js
+++ public/js/application.js
@@ -2,12 +2,16 @@ $(document).ready(function() {
   $(".icon_img").on("click", function(event){
     var img_meme = event.target;
     var img_meme_parent = $(img_meme).parent();
+    
     var card = img_meme_parent.parent()
     var image_element = card.find('.joke_big_image > img');
+    
     $(img_meme_parent).hide();
+    
     var src_my_image = $(img_meme).attr('src');
     image_element.attr("src", src_my_image);
-    $("#image_input").attr("value", src_my_image);
+    
+    card.find(".image_input").attr("value", src_my_image);
 
     image_element.parent().show();
   });
